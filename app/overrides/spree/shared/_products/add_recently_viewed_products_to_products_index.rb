5f31dab71390c598a5e2c0b09c0392ea48a0c84a
Mouhannad <s3749653@student.rmit.edu.au>
2020-11-17 12:50:20 +1100

    deface overried

--- app/overrides/spree/shared/_products/add_recently_viewed_products_to_products_index.rb
+++ app/overrides/spree/shared/_products/add_recently_viewed_products_to_products_index.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/shared/_products',
   name: 'add_recently_viewed_products_to_products_index',
-  insert_after: "#products[data-hook]",
+  insert_after: "#description[data-hook]",
   partial: 'spree/shared/add_recently_viewed_products'
 )
